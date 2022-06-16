7a847fc90463d046418e5a8ef1ebd2978e53d8db
hystolytc <melife4@gmail.com>
2022-06-16 09:23:43 +0700

    refactor: update implementation

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -13,7 +13,7 @@ const Login = () => {
   const [isIncorrect, setIsIncorrect] = useState(false)
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (isIncorrect) setIsIncorrect(!isIncorrect)
+    if (isIncorrect) setIsIncorrect(false)
     if (e.target.name === 'username') return setUsername(e.target.value)
     setPassword(e.target.value)
   }
@@ -21,7 +21,7 @@ const Login = () => {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (!(usernameValidator(username) && passwordValidator(password)))
-      return setIsIncorrect(!isIncorrect)
+      return setIsIncorrect(true)
     
     setLocalStorage.userToken(username+password)
     navigate('/', { replace: true })
@@ -31,13 +31,13 @@ const Login = () => {
     return <Navigate to='/' replace />
   
   return (
-    <div className='relative flex justify-center items-center w-screen h-screen bg-primary'>
+    <div className='login-container relative flex justify-center items-center w-screen h-screen bg-primary'>
       <img 
         className='absolute top-4 left-4 w-80 object-contain'
         src={logo}
         alt='company logo' />
 
-      <section className='px-20 py-16 rounded-lg w-[480px] bg-white'> 
+      <section className='login-box px-20 py-16 rounded-lg w-[480px] bg-white'> 
         <h1 className='font-body font-semibold text-[40px] text-txtPrimary text-center mb-2'>Welcome</h1>
         <p className='text-lg text-txtSecondary font-light text-center mb-5'>Log in to your CASEOS account!</p>
         
@@ -65,7 +65,7 @@ const Login = () => {
           />
 
           <input
-            className='px-4 py-3 bg-primary font-medium text-white text-body mt-4'
+            className='px-4 py-3 bg-primary font-medium text-white text-body mt-4 cursor-pointer hover:opacity-80'
             type='submit'
             value='Log In' />
         </form>
